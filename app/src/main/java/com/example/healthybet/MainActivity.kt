f668164ee916f7d10516e6413bd1f22f282e56a6
Manon Wimmer <manonwimmer30@gmail.com>
2026-09-22 15:29:21 -0400

    [ADD] Home screen tests with compose

--- app/src/main/java/com/example/healthybet/MainActivity.kt
+++ app/src/main/java/com/example/healthybet/MainActivity.kt
@@ -5,14 +5,29 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.savedstate.SavedState
+import androidx.compose.ui.unit.dp
 import com.example.healthybet.ui.theme.HealthyBetTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,9 +38,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             HealthyBetTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    Header(
+                        name = "Healthy Bet",
+                        modifier = Modifier.padding(innerPadding).fillMaxWidth().background(Color.DarkGray)
                     )
                 }
             }
@@ -64,17 +79,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Header(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+
+        val textModifier = Modifier.padding(PaddingValues(30.dp));
+
+        Text(text = name, color= Color.White, modifier=textModifier, textAlign = TextAlign.Center);
+
+        val imageModifier = Modifier.size(150.dp).padding(PaddingValues(10.dp));
+
+        Image(painterResource(
+            id=R.drawable.cat),
+            contentDescription = "cat",
+            modifier = imageModifier
+        );
+
+        val textFieldModifier = Modifier.padding(PaddingValues( 5.dp))
+
+        TextField(
+            state = rememberTextFieldState(),
+            placeholder = { Text("Enter Username") },
+            modifier = textFieldModifier
+        )
+        TextField(
+            state = rememberTextFieldState(),
+            placeholder = { Text("Enter Password") },
+            modifier = textFieldModifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HeaderPreview() {
     HealthyBetTheme {
-        Greeting("Android")
+        Header("Healthy Bet")
     }
 }
